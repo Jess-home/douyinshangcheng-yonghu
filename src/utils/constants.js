b5b08900e4fd64ce3a1538e6071cae1b707d5a69
gujun <gujunchinawuxi@126.com>
2024-04-25 18:16:29 +0800

    2024-04-25

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,7 @@ export const order_statuses= [
     {name:'已发货',value:'2'},
     {name:'已收货',value:'3'},
     {name:'已完成',value:'4'},
-    {name:'已取消',value:'-1'}
+    {name:'已取消',value:'-1'},
+    //  虚拟状态
+    {name:'售后中',value:'-4'}
 ];
